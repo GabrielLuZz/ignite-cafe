53b3de6ad40200d967cf50648df60e8583982e24
Gabriel Luz <gabrielluzsantosl@gmail.com>
2023-06-26 23:45:00 -0300

    feat: global styling done

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,4 +6,19 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme.colors['yellow-dark']} ;
+  }
+
+  body {
+    background-color: ${(props) => props.theme.colors.background};
+    color: ${(props) => props.theme.colors['base-text']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font: ${(props) => props.theme.fonts.font1.TextS};
+  }
 `
--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,3 +1,43 @@
 export const defaultTheme = {
-  primary: '',
+  colors: {
+    white: '#FFFFFF',
+
+    'base-title': '#272221',
+    'base-subtitle': '#403937',
+    'base-text': '#574F4D',
+    'base-label': '#8D8686',
+    'base-hover': '#D7D5D5',
+    'base-button': '#E6E5E5',
+    'base-input': '#EDEDED',
+    'base-card': '#F3F2F2',
+    background: '#FAFAFA',
+
+    'yellow-light': '#F1E9C9',
+    yellow: '#DBAC2C',
+    'yellow-dark': '#C47F17',
+
+    'purple-light': '#EBE5F9',
+    purple: '#8047F8',
+    'purple-dark': '#4B2995',
+  },
+  fonts: {
+    font1: {
+      TextXS: '700 0.75rem/1.3 "Roboto", sans-serif',
+      TextS: '400 0.875rem/1.3 "Roboto", sans-serif',
+      TextMReg: '400 1rem/1.3 "Roboto", sans-serif',
+      TextMBol: '700 1rem/1.3 "Roboto", sans-serif',
+      TextLReg: '400 1.25rem/1.3 "Roboto", sans-serif',
+      TextLBol: '700 1.25rem/1.3 "Roboto", sans-serif',
+      Tag: '700 0.625rem/1.3 "Roboto", sans-serif',
+      ButtonM: '400 0.75rem/1.6 "Roboto", sans-serif',
+      ButtonG: '700 0.875rem/1.6 "Roboto", sans-serif',
+    },
+    font2: {
+      TextXS: '800 1.125rem/1.3 "Baloo 2", cursive',
+      TextS: '800 1.25rem/1.3 "Baloo 2", cursive',
+      TextM: '700 1.5rem/1.3 "Baloo 2", cursive',
+      TextL: '700 2rem/1.3 "Baloo 2", cursive',
+      TextXL: '700 3rem/1.3 "Baloo 2", cursive',
+    },
+  },
 }
